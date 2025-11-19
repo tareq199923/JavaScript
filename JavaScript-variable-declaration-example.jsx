0e63c463ebbd69863d662820917a13a49b9181e8
tareq <109756973+tareq199923@users.noreply.github.com>
2025-11-19 18:12:34 +0600

    Added React example showing JavaScript variable declaration using const and let
    Added a simple React component demonstrating variable declaration with const and let, updating values, and rendering results in JSX.

---
+++ JavaScript-variable-declaration-example.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+const x = 30
+let y = 20
+y = 50
+
+console.log(x,y)
+  return(
+  
+    <div>
+      <p>
+       x plus y is = {x + y}
+      </p>
+      </div>
+
+  )
+}
+export default App
