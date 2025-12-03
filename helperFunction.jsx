1c621ebcf53401bf22d58a8c040be7179a770c8c
tareq <109756973+tareq199923@users.noreply.github.com>
2025-12-03 17:22:30 +0600

    “Add Hello component with helper function to calculate birth year”
    use of helper function

---
+++ helperFunction.jsx
@@ -0,0 +1,33 @@
+const Hello = (props) => {
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+   return yearNow - props.age
+  }
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>
+       So you were probably born {bornYear()}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  )
+}
+
+export default App
